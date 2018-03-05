b71ab45689d52509f6df14482176f9c0284bca48
christopherbolt <chris@christopherbolt.com>
2018-03-05 14:27:46 +1300

    S4 compatibility

--- README.md
+++ README.md
@@ -3,6 +3,13 @@ Publishes/Un-publishes versioned DataObjects that are attached to a Page when th
 
 Lets imagine that you have a staff profiles page where each staff member is a DataObject in a has_many relation. Without any versioning edits to a staff member would be published immediately breaking workflow approval processes and breaking the draft, publish and history functionality of Silverstripe. Or if they did have the Versioned extension they would need to be published independantly from the page which complicates workflow. This module allows those edits to be published when the page is published. Also supports un-publishing, revert to live and history rollback with the page. This allows objects to behave as if they are part of the page.
 
+## Upgrading from SilverStripe 3 ##
+Most of the functionality of this plugin is now built into SilverStripe 4 and can be enabled by using the $owns config that is baked into SS4. To upgrade please replace the $publish_with_me config with $owns.
+In SS4 all this plugin does is enhance the Saved / Published button to show that the page needs publishing if any $owns objects have unpublished changes.
+
+## Requirements ##
+SS 4.x, see 1.0 branch for SS 3.x
+
 ## Usage ##
 Add the PublishWithMe extension to your page (or parent DataObject):
 ```
@@ -10,24 +17,22 @@ private static $extensions = array(
     "PublishWithMe"
 );
 ```
-Still on your page (or parent DataObject) define which relations should be managed by this extension, this is a list of array keys from your has_one and/or has_many relations:
+Still on your page (or parent DataObject) define which relations should be managed by this extension, this is a list of array keys from your has_one and/or has_many relations, this just now uses the $owns config that is built into SS4:
 ```
-private static $publish_with_me = array(
+private static $owns = array(
     'Staff',
 );
 ```	
 The DataObjects to be published with the page must have the Versioned extension, in the example above you would add this to your StaffMember object:
 ```
 private static $extensions = array(
-    "Versioned('Stage', 'Live')",
+    Versioned::class,
 );
 ```
-If your dataobjects themselves contain relations that should be published with the page then also add the PublishWithMe extension and the publish_with_me config and ensure that the child data objects have the Versioned extension etc.
+If your dataobjects themselves contain relations that should be published with the page then also add the PublishWithMe extension and the $owns config and ensure that the child data objects have the Versioned extension etc.
 
 ### Installation ###
 ```
-composer require christopherbolt/silverstripe-publishwithme
+composer require christopherbolt/silverstripe-publishwithme ^2
 ```
 
-### Credits ###
-Silverstripe's UserForms module provided the starting point.
--- src/PublishWithMe.php
+++ src/PublishWithMe.php
@@ -1,11 +1,17 @@
 <?php
 
+namespace ChristopherBolt\PublishWithMe;
+
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Versioned\Versioned;
+
 /**
  * PublishWithMe
  * Publishes/Unpublishes versioned DataObjects that are attached to a Page or parent DataObject when the parent object is published.
- * DataObjects must have the "Versioned('Stage', 'Live')" extension.
+ * DataObjects must have the "Versioned" extension.
  * Your DataObjects can also have optional versioned functions beginning with "do" that will be called instead of the normal versioning functions. e.g. doPublish
- * The parent must have a $publish_with_me config variable listing the relationships to be published/unpublished etc.
+ * The parent must have a $owns config variable listing the relationships to be published/unpublished etc.
  *
  * @package publishobjectswithme
  * @license BSD License http://www.silverstripe.org/bsd-license
@@ -14,7 +20,7 @@
 
 class PublishWithMe extends DataExtension {
 	
-	private static $publish_with_me = array();
+	private static $owns = array();
 	
 	/**
 	 * Builds an array of objects to manage
@@ -26,8 +32,9 @@ class PublishWithMe extends DataExtension {
 		//$this->owner->flushCache();// Ensure that results are not cached
 		$objects = array(); // list of items to manage
 		$item = $this->owner;
-		$fields = $item->config()->get('publish_with_me');
+		$fields = $item->config()->get('owns');
 		if ($fields && is_array($fields) && count($fields)) {
+			
 			$has_one = $item->config()->get('has_one');
 			$has_many = $item->config()->get('has_many');
 			$many_many = $item->config()->get('many_many');
@@ -52,68 +59,6 @@ class PublishWithMe extends DataExtension {
 			}
 		}
 		return $objects;
-	}
-	
-	/**
-	 * @see SiteTree::doPublish
-	 * @param Page $original
-	 *
-	 * @return void
-	 */
-	public function onAfterPublish($original=null) {
-		
-		if ($this->owner->isChanged('ClassName')) return; // We cannot proceed if page type has changed
-		
-		// store all stage items
-		$stage_item_ids = array();
-		$oldMode = Versioned::get_reading_mode();
-		
-		// Publish items to Live stage	
-		Versioned::set_reading_mode('Stage.Stage');	
-		foreach($this->getItemsToPublish() as $field) {
-			$stage_item_ids[] = $field->ClassName.'_'.$field->ID;
-			if ($field->hasMethod('doPublish')) {
-				$field->doPublish('Stage', 'Live');
-			} else {
-				$field->publish('Stage', 'Live');
-			}
-			// if this object also has this extension then do cascade
-			if ($field->hasMethod('onAfterPublish')) {
-				$field->onAfterPublish($field);
-			}
-		}
-		
-		// Remove any data on the live table that was deleted from the draft
-		Versioned::set_reading_mode('Stage.Live');
-		foreach($this->getItemsToPublish() as $field) {
-			if (!in_array($field->ClassName.'_'.$field->ID, $stage_item_ids)) {
-				if ($field->hasMethod('doDeleteFromStage')) {
-					$field->doDeleteFromStage('Live');
-				} else {
-					$field->deleteFromStage('Live');
-				}
-			}
-		}
-		
-		// reset mode
-		Versioned::set_reading_mode($oldMode);
-	}
-
-	/**
-	 * @see SiteTree::doUnpublish
-	 * @param Page $page
-	 *
-	 * @return void
-	 */
-	public function onAfterUnpublish($page) {
-		if (!$page->hasMethod('getItemsToPublish')) return;		
-		foreach($page->getItemsToPublish() as $field) {
-			if ($field->hasMethod('doDeleteFromStage')) {
-				$field->doDeleteFromStage('Live');
-			} else {
-				$field->deleteFromStage('Live');
-			}
-		}
 	}
 
 	/**
@@ -161,75 +106,14 @@ class PublishWithMe extends DataExtension {
 	}
 	
 	/**
-	 * @see Versioned::doRollBackTo
-	 * @param string $version Either the string 'Live' or a version number
-	 *
-	 * @return null
-	 */
-	public function onAfterRollback($version) {
-		// get the owner data for this version	
-		if(is_numeric($version)) {
-			$page = Versioned::get_version($this->owner->ClassName, $this->owner->ID, $version);
-		} else {
-			$this->owner->flushCache();
-			$baseTable = $this->owner->baseTable();	
-			$page = Versioned::get_one_by_stage($this->owner->ClassName, $version, "\"{$baseTable}\".\"ID\"={$this->owner->ID}");
-		}
-				
-		$date = $page->LastEdited;
-		$oldMode = Versioned::get_reading_mode();
-		
-		// get the current items
-		Versioned::set_reading_mode('Stage.Stage');
-		$current_items = array();
-		foreach($this->getItemsToPublish() as $field) {
-			$current_items[] = $field;
-		}
-		
-		// Get items in the version that we want by setting reading mode to date of version
-		if(is_numeric($version)) {
-			Versioned::set_reading_mode('Archive.' . $date);
-		} else {
-			Versioned::set_reading_mode('Stage.Live');
-		}
-		$version_items = array();
-		$version_ids = array();
-		foreach($this->getItemsToPublish() as $field) {
-			$version_items[] = $field;
-			$version_ids[] = $field->ClassName.'_'.$field->ID;
-		}
-		Versioned::set_reading_mode($oldMode);
-		
-		// rollback version items
-		foreach ($version_items as $field) {
-			$fieldVersion = is_numeric($version) ? $field->Version : 'Live';
-			if ($field->hasMethod('doDoRollBackTo')) {
-				$field->doDoRollBackTo($fieldVersion);
-			} else {
-				$field->doRollBackTo($fieldVersion);
-			}
-		}
-		
-		// delete items not in version
-		foreach ($current_items as $field) {
-			if (!in_array($field->ClassName.'_'.$field->ID, $version_ids)) {
-				$clone = clone $field;
-				if ($clone->hasMethod('doDeleteFromStage')) {
-					$clone->doDeleteFromStage('Stage');
-				} else {
-					$clone->deleteFromStage('Stage');
-				}
-			}
-		}
-	}
-	
-	/**
 	 * @param FieldList
 	 */
 	public function updateCMSActions(FieldList $fields) {
 		// Update state of publish button if there are unpublished changes in objects
 		if ($this->getIsModifiedOnStage(false) && ($publish = $fields->fieldByName('MajorActions.action_publish'))) {
-			$publish->addExtraClass('ss-ui-alternate');
+			$publish->addExtraClass('btn-primary font-icon-rocket');
+			$publish->removeExtraClass('btn-outline-primary');
+			$publish->setTitle(_t('SilverStripe\CMS\Model\SiteTree.BUTTONSAVEPUBLISH', 'Save & publish'));
 		}
 	}
 
